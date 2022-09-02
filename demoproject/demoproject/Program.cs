767d4fcb3662bfabb31c8b02468056457885967f
anusha <anusweetie.me@gmail.com>
2022-09-03 00:27:56 +0800

    firstcommit

---
+++ demoproject/demoproject/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+IWebElement usernameTextBox = driver.FindElement(By.Id("UserName"));
+usernameTextBox.SendKeys("hari");
+IWebElement passwordTextBox = driver.FindElement(By.Id("Password"));
+passwordTextBox.SendKeys("123123");
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if (hellohari.Text == "Hello hari!") 
+{
+    Console.WriteLine("LOGIN SUCCESSFULL");
+}
+
+else
+{
+    Console.WriteLine("LOGIN FAILED");
+}
+
+
+
